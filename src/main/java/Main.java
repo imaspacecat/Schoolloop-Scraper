19f8ef22394883b6ba181acedf0eddac89a696db
imaspacecat <library4lion@gmail.com>
2022-02-26 23:22:18 -0800

    completely changed functionality

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,32 @@
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.*;
 
-import java.io.*;
-import java.nio.charset.StandardCharsets;
+import java.io.File;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String url = "https://www.nytimes.com/2022/02/25/science/venus-photos-nasa.html";
-        Document doc = Jsoup.connect(url).get();
-        File outputFile = new File("C:\\Users\\yotam\\Desktop\\output.html");
-        FileWriter writer = new FileWriter("output.html");
-        writer.write(new String(doc.html().getBytes(StandardCharsets.UTF_16)));
-        writer.close();
+        final String username = "yotamdubiner";
+        final String password = "bcatnumber1";
+        String url = "https://wilcox.schoolloop.com/portal/login";
 
-        // extract the text from the class "css-axufdj evys1bk0" of the <p> element
+        final WebClient webClient = new WebClient();
+        final HtmlPage loginPage = webClient.getPage(url);
+        final HtmlForm loginForm = loginPage.getFormByName("form");
+
+        HtmlTextInput userField = loginForm.getInputByName("login_name");
+        HtmlPasswordInput passField = loginForm.getInputByName("password");
+
+        userField.type(username);
+        passField.type(password);
+
+        HtmlAnchor htmlAnchor = loginPage.getAnchorByHref("javascript:document.form.event_override.value='login';document.form.submit();");
+        HtmlPage inboxPage = htmlAnchor.click();
+
+        inboxPage.save(new File("inbox.html"));
+
+        System.out.println(inboxPage.asXml());
+
     }
 
 
